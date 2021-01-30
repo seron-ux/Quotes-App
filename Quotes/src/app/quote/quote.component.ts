d1e65052b16bc1908f54e37df894b68b3e6c1ca1
Seron-ux <abyseronkirui@gmail.com>
2021-01-30 12:30:21 +0300

    added a h1

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -22,7 +22,7 @@ export class QuoteComponent implements OnInit {
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     quote.completeDate = new Date(quote.completeDate)
-    this.goals.push(quote)
+    this.quotes.push(quote)
   }
 
   deleteQuote(isComplete:any, index:any){
